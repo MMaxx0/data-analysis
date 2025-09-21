6819655fb9dae5d4d087be0ca4179c118521dc73
jp1593 <juanpablo08082002@hotmail.com>
2025-09-21 14:10:33 -0600

    feat: add logic for two players

--- texas-holdem/multiple_bets.py
+++ texas-holdem/multiple_bets.py
@@ -1,23 +1,35 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
-# Probabilidades y ganancias/pérdidas
-outcomes = np.array([1, -5, 5, -100])   # ganancias por mano
+# Possible results and probabilities
+outcomes = np.array([1, -5, 5, -100])   
 probs = np.array([0.70, 0.10, 0.15, 0.05])
 
-def session(manos=100, stack_inicial=100):
-    stack = stack_inicial
-    historial = [stack]
+def session(hands=100, player_1=100, player_2=100):
+    stack_p1 = player_1
+    stack_p2 = player_2
+    historial_p1 = [stack_p1]
+    historial_p2 = [stack_p2]
     
-    for _ in range(manos):
-        cambio = np.random.choice(outcomes, p=probs)
-        stack += cambio
-        historial.append(stack)
+    for _ in range(hands):
+        change = np.random.choice(outcomes, p=probs)
+        print(change)
+        if (change > 0): 
+            stack_p1 += change
+            stack_p2 -= change
+            historial_p1.append(stack_p1)
+            historial_p2.append(stack_p2)
+            outcomes[-1] = stack_p1
+        else: 
+            stack_p1 -= change
+            stack_p2 += change
+            historial_p1.append(stack_p1)
+            historial_p2.append(stack_p2)
+            outcomes[-1] = stack_p1
         
-        if stack <= 0: 
+        if stack_p1 <= 0 or stack_p2 <= 0:  
             break
     
-    print(np.array(historial))
+    print ("\nPLayer 1: \n", np.array(historial_p1), "\nPlayer 2: \n", np.array(historial_p2))
 
-# Simulación de varias sesiones
-reuslt = session()
+# 1 Session
+result = session()
