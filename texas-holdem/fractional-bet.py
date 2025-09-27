3544bd6e978be65a6f4fb346210813ea52b36b05
Max <145176556+MMaxx0@users.noreply.github.com>
2025-09-26 21:21:48 -0600

    feat: fractional bet hands

--- texas-holdem/fractional-bet.py
+++ texas-holdem/fractional-bet.py
@@ -40,75 +40,69 @@ def run_session():
     return session_data
 
 
-def run_simulation(session_count=10000):
+def run_simulation(count=10000):
     total_won_hands = 0
     total_hands_played = 0
-    won_hands_counts = []
+    won_hands_per_session = []
     total_bankruptcies = 0
 
-    for _ in range(session_count):
+    for _ in range(count):
         session_stats = run_session()
 
         total_won_hands += session_stats["won_hands"]
         total_hands_played += session_stats["hands_played"]
-        won_hands_counts.append(session_stats["won_hands"])
+        won_hands_per_session.append(session_stats["won_hands"])
 
         if session_stats["went_bankrupt"]:
             total_bankruptcies += 1
 
     # Following the Binomial distribution
-    # Use actual average hands per session to account for bankruptcies
-    n = total_hands_played / session_count  # actual average hands per session
+    n = 100  # sequence of n independent yes/no experiments
     p = 0.5  # Probability of yes
 
     # Theoretical values
-    per_session_theoretical_expected_value = n * p
-    per_session_theoretical_variance = n * p * (1 - p)
-    # Bankruptcy
-    per_session_bankruptcy_probability = (1 - p) ** n
+    theoretical_mean = n * p
+    theoretical_variance = n * p * (1 - p)
 
     # Simulated values
-    mean_per_session = total_won_hands / session_count
-    squared_diffs = [
-        (session_wins - mean_per_session) ** 2 for session_wins in won_hands_counts
-    ]
-    per_session_variance = sum(squared_diffs) / (session_count - 1)
+    mean = sum(won_hands_per_session) / count
+    sum_squared_deviations = sum((x - mean) ** 2 for x in won_hands_per_session)
+    variance = sum_squared_deviations / (count - 1)
 
     print(f"Total hands played: {total_hands_played}")
-    print(f"Theoretical Expected value: {per_session_theoretical_expected_value}")
-    print(f"Total hands won: {total_won_hands}")
-    print(f"Simulated Expected value: {mean_per_session:.4f}")
-    print(f"Theoretical variance: {per_session_theoretical_variance:.4f}")
-    print(f"Simulated variance: {per_session_variance:.4f}")
-    print(
-        f"Probability of bankruptcy in a session: {per_session_bankruptcy_probability:.2e}"
-    )
-    print(f"Total bankruptcies: {total_bankruptcies} out of {session_count} sessions")
+    print(f"Theoretical Mean: {theoretical_mean}")
+    print(f"Theoretical Variance: {theoretical_variance}")
+    print(f"Simulated Mean: {mean}")
+    print(f"Simulated Variance: {variance}")
 
     # Plotting
     fig, axs = plt.subplots(1, 1, figsize=(12, 10))
 
     axs.hist(
-        won_hands_counts, bins="auto", color="skyblue", edgecolor="black", alpha=0.7
+        won_hands_per_session,
+        bins="auto",
+        color="skyblue",
+        edgecolor="black",
+        alpha=0.7,
     )
-    per_session_std_dev = per_session_variance**0.5
+    per_session_std_dev = variance**0.5
 
     axs.axvline(
-        per_session_theoretical_expected_value,
+        theoretical_mean,
         color="red",
         linestyle="dashed",
         linewidth=2,
-        label="Theoretical Mean (per session)",
+        label="Theoretical Mean",
     )
     axs.axvline(
-        mean_per_session + per_session_std_dev,
+        mean + per_session_std_dev,
         color="green",
         linestyle="dotted",
         linewidth=2,
         label="+1 Std Dev",
     )
     axs.axvline(
-        mean_per_session - per_session_std_dev,
+        mean - per_session_std_dev,
         color="green",
         linestyle="dotted",
         linewidth=2,
@@ -120,11 +114,15 @@ def run_simulation(session_count=10000):
     axs.grid(axis="y", linestyle="--", alpha=0.7)
     axs.legend()
 
-    theoretical_per_session_text = f"""**Theoretical (Per Session)**\nMean: {per_session_theoretical_expected_value:.4f}\nVariance: {per_session_theoretical_variance:.4f}\nBankruptcy probability: {per_session_bankruptcy_probability:.2e}"""
-    simulated_per_session_text = f"""**Simulated (Per Session)**\nMean: {mean_per_session:.4f}\nVariance: {per_session_variance:.4f}\nBankruptcies: {total_bankruptcies}"""
+    general_stats_text = f"""**General Stats**\nTotal Hands Played: {total_hands_played}\nTotal Hands Won: {total_won_hands}\nTotal Bankruptcies: {total_bankruptcies}"""
+    theoretical_per_session_text = f"""**Theoretical**\nMean: {theoretical_mean:.4f}\nVariance: {theoretical_variance:.4f}\n"""
+    simulated_per_session_text = (
+        f"""**Simulated**\nMean: {mean:.4f}\nVariance: {variance:.4f}\n"""
+    )
 
-    add_stats_box(axs, 0.98, 0.98, theoretical_per_session_text, "#e0e0ff", "navy")
-    add_stats_box(axs, 0.80, 0.98, simulated_per_session_text, "#ffe0e0", "darkred")
+    add_stats_box(axs, 0.20, 0.98, general_stats_text, "#e0ffe0", "green")
+    add_stats_box(axs, 0.32, 0.98, simulated_per_session_text, "#ffe0e0", "darkred")
+    add_stats_box(axs, 0.445, 0.98, theoretical_per_session_text, "#e0e0ff", "navy")
     plt.tight_layout()
     plt.show()
 
