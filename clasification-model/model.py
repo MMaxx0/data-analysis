4435bdea100236eb8a573ca48d256b40907151d2
jp1593 <juanpablo08082002@hotmail.com>
2025-09-27 18:30:01 -0600

    feat: amount of rows and columns + datatypes

--- clasification-model/model.py
+++ clasification-model/model.py
@@ -6,6 +6,9 @@ zip_path = "poker+hand.zip"
 
 # Cols
 cols = ['S1','C1','S2','C2','S3','C3','S4','C4','S5','C5','hand']
+# S = Palo de la carta 
+# C = Valor de la carta
+# hand = Clase de mano (0 = valor más alto, 1 = par, etc...)
 
 with zipfile.ZipFile(zip_path) as z:
     with z.open("poker-hand-training-true.data") as f:
@@ -13,6 +16,11 @@ with zipfile.ZipFile(zip_path) as z:
     with z.open("poker-hand-testing.data") as f:
         test_df = pd.read_csv(f, names=cols)
 
-# Revisar
 print("\n",train_df.head())
 print("\n", test_df.head())
+
+# Dimensiones + Tipos de datos
+print("\nTrain shape:", train_df.shape)
+print("\nTest shape:", test_df.shape)
+# X filas, 11 Columnas
+print(f"\n{train_df.dtypes}")
